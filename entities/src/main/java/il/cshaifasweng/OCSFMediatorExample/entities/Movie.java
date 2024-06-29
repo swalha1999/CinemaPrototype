964a5c721b373e2740b1a178ac85fb637ba76de8
omar <omarrayan1999@gmail.com>
2024-06-29 22:37:48 +0300

    added a Movie class

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Movie.java
@@ -0,0 +1,42 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private Date date;
+
+    public Movie(){}
+    public Movie(String name, Date date) {
+        this.name = name;
+        this.date = date;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
