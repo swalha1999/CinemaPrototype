434ced9a60f40e5a97ebfaf35a3b64151e0818ad
swalha1999 <mohamd514.m@gmail.com>
2024-06-30 21:56:14 +0300

    added Eventbus methods to the primery controller

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
@@ -11,6 +11,10 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.io.IOException;
 
 import static il.cshaifasweng.OCSFMediatorExample.client.SimpleChatClient.setRoot;
@@ -34,6 +38,27 @@ public class PrimaryController {
 
     @FXML // fx:id="table_users"
     private TableView<?> table_users; // Value injected by FXMLLoader
+
+    @FXML
+    public void initialize() {
+        EventBus.getDefault().register(this);
+        AddBtn.setOnAction(event -> EventBus.getDefault().post(new MessageEvent("Hello from EventBus!"))); // replace with server calls
+        DeleteBtn.setOnAction(event -> EventBus.getDefault().postSticky(new StickyMessageEvent("Sticky Hello from EventBus!"))); // replace with server calls
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        System.out.println(event.getMessage());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onStickyMessageEvent(StickyMessageEvent event) {
+        EventBus.getDefault().removeStickyEvent(event);
+    }
+
+    public void cleanup() {
+        EventBus.getDefault().unregister(this);
+    }
 
     @FXML
     void AddMovies(ActionEvent event) throws IOException {
