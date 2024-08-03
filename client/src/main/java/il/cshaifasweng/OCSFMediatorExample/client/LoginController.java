526a7883734bc17039b156e4747cdf317c715d1b
abedassi2001 <Abedassi134@gmail.com>
2024-08-03 22:35:38 +0300

    /**  * Sample Skeleton for 'Register.fxml' Controller Class  */
    package il.cshaifasweng.OCSFMediatorExample.client;
    
    import javafx.event.ActionEvent;
    import javafx.fxml.FXML;
    import javafx.scene.control.Button;
    import javafx.scene.control.Label;
    import javafx.scene.control.TextField;
    import javafx.scene.image.ImageView;
    
    public class RegisterController {
    
        @FXML // fx:id="ConfPasswordLabel"
        private Label ConfPasswordLabel; // Value injected by FXMLLoader
    
        @FXML // fx:id="ConfPasswordText"
        private TextField ConfPasswordText; // Value injected by FXMLLoader
    
        @FXML // fx:id="ConfirmationBut"
        private Button ConfirmationBut; // Value injected by FXMLLoader
    
        @FXML // fx:id="ConfirmationBut1"
        private Button ConfirmationBut1; // Value injected by FXMLLoader
    
        @FXML // fx:id="FirstNameText"
        private TextField FirstNameText; // Value injected by FXMLLoader
    
        @FXML // fx:id="LastNameLabel"
        private Label LastNameLabel; // Value injected by FXMLLoader
    
        @FXML // fx:id="LastNameText"
        private TextField LastNameText; // Value injected by FXMLLoader
    
        @FXML // fx:id="PasswordLabel"
        private Label PasswordLabel; // Value injected by FXMLLoader
    
        @FXML // fx:id="PasswordText"
        private TextField PasswordText; // Value injected by FXMLLoader
    
        @FXML // fx:id="RegisterLogo"
        private Label RegisterLogo; // Value injected by FXMLLoader
    
        @FXML // fx:id="UserNameLabel"
        private Label UserNameLabel; // Value injected by FXMLLoader
    
        @FXML // fx:id="UserNameText"
        private TextField UserNameText; // Value injected by FXMLLoader
    
        @FXML // fx:id="chechMark"
        private ImageView chechMark; // Value injected by FXMLLoader
    
        @FXML // fx:id="firstNameLabel"
        private Label firstNameLabel; // Value injected by FXMLLoader
    
        @FXML
        void ConfrimRegister(ActionEvent event) {
    
        }
    
    }
    hi

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LoginController.java
@@ -0,0 +1,39 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class LoginController {
+
+    @FXML
+    private Button LoginBtn;
+
+    @FXML
+    private Text LoginText;
+
+    @FXML
+    private PasswordField Password;
+
+    @FXML
+    private ImageView PasswordFont;
+
+    @FXML
+    private ImageView UserFont;
+
+    @FXML
+    private PasswordField UserName;
+
+    @FXML
+    private Hyperlink forgotPassword;
+
+    @FXML
+    void LoginAccount(ActionEvent event) {
+
+    }
+
+}
---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/RegisterController.java
@@ -0,0 +1,63 @@
+/**
+ * Sample Skeleton for 'Register.fxml' Controller Class
+ */
+
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class RegisterController {
+
+    @FXML // fx:id="ConfPasswordLabel"
+    private Label ConfPasswordLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ConfPasswordText"
+    private TextField ConfPasswordText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ConfirmationBut"
+    private Button ConfirmationBut; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ConfirmationBut1"
+    private Button ConfirmationBut1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="FirstNameText"
+    private TextField FirstNameText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="LastNameLabel"
+    private Label LastNameLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="LastNameText"
+    private TextField LastNameText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="PasswordLabel"
+    private Label PasswordLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="PasswordText"
+    private TextField PasswordText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="RegisterLogo"
+    private Label RegisterLogo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="UserNameLabel"
+    private Label UserNameLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="UserNameText"
+    private TextField UserNameText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="chechMark"
+    private ImageView chechMark; // Value injected by FXMLLoader
+
+    @FXML // fx:id="firstNameLabel"
+    private Label firstNameLabel; // Value injected by FXMLLoader
+
+    @FXML
+    void ConfrimRegister(ActionEvent event) {
+
+    }
+
+}
