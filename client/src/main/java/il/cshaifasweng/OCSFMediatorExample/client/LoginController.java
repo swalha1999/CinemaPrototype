5df7bde4c4ad4c484950ed1948e904f947276a16
omar ryyan <omarrayan1999@gmail.com>
2024-08-06 15:16:11 +0300

    fixed exception in Login fxml + added transition from login to catalog

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LoginController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LoginController.java
@@ -1,46 +1,58 @@
+/**
+ * Sample Skeleton for 'Login.fxml' Controller Class
+ */
+
 package il.cshaifasweng.OCSFMediatorExample.client;
 
 import il.cshaifasweng.OCSFMediatorExample.entities.Message;
 import il.cshaifasweng.OCSFMediatorExample.entities.User;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-import java.awt.*;
 import java.io.IOException;
 
 import static il.cshaifasweng.OCSFMediatorExample.client.SimpleChatClient.setRoot;
+
 public class LoginController {
 
-    @FXML
-    private Button LoginBtn;
+    @FXML // fx:id="LoginBtn"
+    private Button LoginBtn; // Value injected by FXMLLoader
 
-    @FXML
-    private Text LoginText;
+    @FXML // fx:id="LoginText"
+    private Text LoginText; // Value injected by FXMLLoader
 
-    @FXML
-    private PasswordField Password;
+    @FXML // fx:id="Password"
+    private PasswordField Password; // Value injected by FXMLLoader
 
-    @FXML
-    private Button RegisterNewAccount;
+    @FXML // fx:id="RegisterNewAccount"
+    private Button RegisterNewAccount; // Value injected by FXMLLoader
 
-    @FXML
-    private TextField UserNameTxt;
+    @FXML // fx:id="UserNameTxt"
+    private TextField UserNameTxt; // Value injected by FXMLLoader
 
-    @FXML
-    private Hyperlink forgotPassword;
+    @FXML // fx:id="forgotPassword"
+    private Hyperlink forgotPassword; // Value injected by FXMLLoader
 
     @FXML
     void LoginAccount(ActionEvent event) throws IOException {
         Message message = new Message(2, "login");
         message.setUser(new User());
-        message.getUser().setUsername(UserNameTxt.getText());
+        //message.getUser().setUsername(UserNameTxt.getText());
         message.setData(Password.getText());
         SimpleClient.getClient().sendToServer(message);
+        Platform.runLater(()->{
+            try {
+                setRoot("primary");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
 }
