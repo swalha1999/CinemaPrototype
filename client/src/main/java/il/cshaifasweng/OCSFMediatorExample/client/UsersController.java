bc05b5129ef6eaa1f6e490fe204ca4e77ce05c0e
omar ryyan <omarrayan1999@gmail.com>
2024-08-06 15:26:21 +0300

    added UsersController

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/UsersController.java
@@ -0,0 +1,61 @@
+/**
+ * Sample Skeleton for 'UsersController.fxml' Controller Class
+ */
+
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class UsersController {
+
+    @FXML // fx:id="adminBtn"
+    private Button adminBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="customerServiceBtn"
+    private Button customerServiceBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="deleteBtn"
+    private Button deleteBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="releaseBtn"
+    private Button releaseBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="suspendBtn"
+    private Button suspendBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="userBtn"
+    private Button userBtn; // Value injected by FXMLLoader
+
+    @FXML
+    void Delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Release(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Suspend(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createAdmin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createCustomerService(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createUser(ActionEvent event) {
+
+    }
+
+}
