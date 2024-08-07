5dfe3b788d5a497e4c6307f0511132703e9a2b2c
abedassi2001 <Abedassi134@gmail.com>
2024-08-07 22:18:11 +0300

    added getters and setters to the unblock massege

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/UnblockRequest.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/UnblockRequest.java
@@ -1,20 +1,58 @@
 package il.cshaifasweng.OCSFMediatorExample.entities.messages;
 
+import java.io.Serial;
 import java.io.Serializable;
 
-public class UnblockRequest implements Serializable {
+public class UnblockRequest implements Request {
+    @Serial
     private static final long serialVersionUID = 1L;
     private String message;
+    private String SessionKey;
+    private String UserName;
+    private int UserId;
     public UnblockRequest(String message) {
         this.message = message;
     }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
+   public UnblockRequest(String message, String SessionKey, String UserName) {
         this.message = message;
-    }
-    public String toString() {
+        this.SessionKey = SessionKey;
+        this.UserName = UserName;
+   }
+   public String getMessage() {
         return message;
+   }
+   public void setMessage(String message) {
+        this.message = message;
+   }
+   public String getSessionKey() {
+        return SessionKey;
+   }
+
+    @Override
+    public UnblockRequest setSessionKey(String sessionKey) {
+        SessionKey = sessionKey;
+        return this;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.UserName;
+    }
+
+    @Override
+    public UnblockRequest setUsername(String username) {
+        this.UserName = username;
+        return this;
+    }
+
+    @Override
+    public int getUserId() {
+        return this.UserId;
+    }
+
+    @Override
+    public UnblockRequest setUserId(int userId) {
+        this.UserId = userId;
+        return this;
     }
 }
--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/UnblockResponse.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/UnblockResponse.java
@@ -2,9 +2,10 @@ package il.cshaifasweng.OCSFMediatorExample.entities.messages;
 
 import java.io.Serializable;
 
-public class UnblockResponse implements Serializable {
+public class UnblockResponse implements Response {
     private static final long serialVersionUID = 1L;
     private String message;
+    private Boolean IsSuccess;
     public UnblockResponse(String message) {
         this.message = message;
     }
@@ -15,6 +16,12 @@ public class UnblockResponse implements Serializable {
         this.message = message;
         return this;
     }
+
+    @Override
+    public boolean isSuccess() {
+        return false;
+    }
+
     public String toString() {
         return "UnblockResponse [message=" + message + "]";
     }
