0f835800ef89e1a4f78f5754118e03a9fd9b8804
abedassi2001 <Abedassi134@gmail.com>
2024-08-07 22:08:55 +0300

    Block getters and setters

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/BlockRequest.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/BlockRequest.java
@@ -1,10 +1,14 @@
 package il.cshaifasweng.OCSFMediatorExample.entities.messages;
 
-import java.io.Serializable;
+import java.io.Serial;
 
-public class BlockRequest implements Serializable {
+public class BlockRequest implements Request {
+    @Serial
     private static final long serialVersionUID = 1L;
     private String block;
+    private String username ;
+    private int userid;
+    private String SessionKey;
     public BlockRequest(String block) {
         this.block = block;
     }
@@ -16,5 +20,31 @@ public class BlockRequest implements Serializable {
     }
     public String toString() {
         return block;
+    }
+
+    public String getSessionKey() {
+        return SessionKey;
+    }
+
+    public BlockRequest setSessionKey(String sessionKey) {
+        this.SessionKey = sessionKey;
+        return this;
+    }
+
+    public  String getUsername() {
+        return this.username;
+    }
+
+    public BlockRequest setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public int getUserId() {
+        return userid;
+    }
+    public BlockRequest setUserId(int userId) {
+        this.userid = userId;
+        return this;
     }
 }
