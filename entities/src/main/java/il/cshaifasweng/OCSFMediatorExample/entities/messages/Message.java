2596af86ffdb00c3ed9b89da08b3fa6577725acc
swalha1999 <mohamd514.m@gmail.com>
2024-08-10 07:46:53 +0300

    added V3 message type and cleaned up the message class

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/Message.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/Message.java
@@ -9,33 +9,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Message implements Serializable {
-    int id = 0; // TODO : deprecated field need to be removed
 
     MessageType type = MessageType.NONE;
     MessageVersion version = MessageVersion.V1;
     LocalDateTime timeStamp = LocalDateTime.now();
+    String message = "no message";
     Object dataObject = "No data";
 
-    String message = "no message"; //TODO : deprecated field need to be removed
 
     public Message(Object dataObject, MessageType type) {
         this.version = MessageVersion.V2;
         this.type = type;
-        this.id = type.ordinal();
         this.timeStamp = LocalDateTime.now();
         this.dataObject = dataObject;
     }
 
-    public Message(int id, String message) {
-        this.version = MessageVersion.V1;
-        this.id = id;
+    public Message(MessageType type, MessageVersion version) {
+        this.type = type;
+        this.version = version;
         this.timeStamp = LocalDateTime.now();
-        this.message = message;
-        this.dataObject = "No data";
-    }
-
-    public int getId() {
-        return id;
     }
 
     public LocalDateTime getTimeStamp() {
@@ -46,18 +38,15 @@ public class Message implements Serializable {
         return message;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTimeStamp(LocalDateTime timeStamp) {
+    public Message setTimeStamp(LocalDateTime timeStamp) {
         this.timeStamp = timeStamp;
+        return this;
     }
 
-    public void setMessage(String message) {
+    public Message setMessage(String message) {
         this.message = message;
+        return this;
     }
-
 
     public Object getDataObject() {
         return dataObject;
@@ -78,12 +67,11 @@ public class Message implements Serializable {
 
     public String toString() {
         return "Message{" +
-                "id=" + id +
                 ", type=" + type.toString() +
                 ", version=" + version.toString() +
                 ", timeStamp=" + timeStamp.format( java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss") ) +
-                ", dataObject=" + dataObject.toString() +
                 ", message='" + message + '\'' +
+                ", dataObject=" + dataObject.toString() +
                 '}';
     }
 
--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/MessageVersion.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/messages/MessageVersion.java
@@ -8,6 +8,9 @@ import java.io.Serializable;
  * V1 is the old version of the message. and its deprecated. and every type of message are in the same class and that is not good.
  * V2 is the new version of the message. it contains a dataObject field that can be any object that extends the Serializable interface.
  * this object can be any type of object that the message needs to send. you can see like LoginRequest, LoginResponse, etc.
+ *
+ * V3 the idea after V3 is that we send the entity object itself and not the dataObject. and this will allow to not make a request and response and patch
+ * for every entity.
  */
 public enum MessageVersion implements Serializable {
     V1, // TODO: this needs to DEPRECATED and every new message should be the newish version
