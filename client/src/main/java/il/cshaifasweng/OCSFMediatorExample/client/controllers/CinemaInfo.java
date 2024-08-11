08a878fc0d508f3be6c2737be787af7e55e96c75
abedassi2001 <Abedassi134@gmail.com>
2024-08-11 21:23:33 +0300

    connected the gui of Ciename info with client

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/CinemaInfo.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/CinemaInfo.java
@@ -4,11 +4,27 @@
 
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import il.cshaifasweng.OCSFMediatorExample.client.SimpleClient;
+import il.cshaifasweng.OCSFMediatorExample.client.data.CinemaView;
+import il.cshaifasweng.OCSFMediatorExample.client.data.SessionKeysStorage;
+import il.cshaifasweng.OCSFMediatorExample.client.data.UserView;
+import il.cshaifasweng.OCSFMediatorExample.client.events.GetAllCinemasEvent;
+import il.cshaifasweng.OCSFMediatorExample.entities.dataTypes.Cinema;
+import il.cshaifasweng.OCSFMediatorExample.entities.dataTypes.User;
+import il.cshaifasweng.OCSFMediatorExample.entities.messages.Message;
+import il.cshaifasweng.OCSFMediatorExample.entities.messages.MessageType;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.io.IOException;
+import java.util.List;
 
 public class CinemaInfo {
 
@@ -25,13 +41,13 @@ public class CinemaInfo {
     private Button HomeBtn; // Value injected by FXMLLoader
 
     @FXML // fx:id="cinemaNameColumn"
-    private TableColumn<?, ?> cinemaNameColumn; // Value injected by FXMLLoader
+    private TableColumn<CinemaView, String> cinemaNameColumn; // Value injected by FXMLLoader
 
     @FXML // fx:id="cinemaTable"
-    private TableView<?> cinemaTable; // Value injected by FXMLLoader
+    private TableView<CinemaView> cinemaTable; // Value injected by FXMLLoader
 
     @FXML // fx:id="cityColumn"
-    private TableColumn<?, ?> cityColumn; // Value injected by FXMLLoader
+    private TableColumn<CinemaView, String> cityColumn; // Value injected by FXMLLoader
 
     @FXML // fx:id="detailsTable"
     private TableView<?> detailsTable; // Value injected by FXMLLoader
@@ -44,6 +60,19 @@ public class CinemaInfo {
 
     @FXML // fx:id="seatsColumn"
     private TableColumn<?, ?> seatsColumn; // Value injected by FXMLLoader
+
+    @FXML
+    public void initialize() throws IOException {
+        EventBus.getDefault().register(this); //TODO: add this to all controllers - please :)
+
+
+        Message message = new Message(MessageType.GET_ALL_CINEMAS_REQUEST)
+                .setSessionKey(SessionKeysStorage.getInstance().getSessionKey());
+        SimpleClient.getClient().sendToServer(message);
+   cinemaNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+   cityColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
+   hallColumn.setCellValueFactory(new PropertyValueFactory<>("hall"));
+    }
 
     @FXML
     void addCinema(ActionEvent event) {
@@ -63,6 +92,16 @@ public class CinemaInfo {
     @FXML
     void returnHome(ActionEvent event) {
 
+    }
+    @Subscribe
+    public void onGetAllCinemas(GetAllCinemasEvent event) {
+        Platform.runLater(() -> {
+            List<Cinema> cinemas = event.getCinemas();
+            cinemaTable.getItems().clear();
+            for (Cinema cinema :cinemas ) {
+                cinemaTable.getItems().add(new CinemaView(cinema));
+            }
+        });
     }
 
 }
