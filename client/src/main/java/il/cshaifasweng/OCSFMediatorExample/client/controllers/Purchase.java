14626eae6b69e20f53d51e3b92d83207aef88e34
abedassi2001 <Abedassi134@gmail.com>
2024-08-11 05:58:41 +0300

    done gui

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/Purchase.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/Purchase.java
@@ -15,47 +15,41 @@ public class Purchase {
     @FXML // fx:id="CVV_Txt"
     private TextField CVV_Txt; // Value injected by FXMLLoader
 
-    @FXML // fx:id="ConfrimPurchaseBtn"
-    private Button ConfrimPurchaseBtn; // Value injected by FXMLLoader
+    @FXML // fx:id="ConfirmPurchaseBtn"
+    private Button ConfirmPurchaseBtn; // Value injected by FXMLLoader
 
-    @FXML // fx:id="Duration_Txt"
-    private TextField Duration_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="DurationLabel"
+    private Label DurationLabel; // Value injected by FXMLLoader
 
     @FXML // fx:id="HomeBtn"
     private Button HomeBtn; // Value injected by FXMLLoader
 
-    @FXML // fx:id="MovieDetailsLabel"
-    private Label MovieDetailsLabel; // Value injected by FXMLLoader
-
-    @FXML // fx:id="MovieTime_Txt"
-    private TextField MovieTime_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="MovieTimeLabel"
+    private Label MovieTimeLabel; // Value injected by FXMLLoader
 
     @FXML // fx:id="MovieTitleLabel"
     private Label MovieTitleLabel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="MovieTitle_txt"
-    private TextField MovieTitle_txt; // Value injected by FXMLLoader
+    @FXML // fx:id="NumberItemPurchLabel"
+    private Label NumberItemPurchLabel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="NumberItemPurch_Txt"
-    private TextField NumberItemPurch_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="OrderIdLabel"
+    private Label OrderIdLabel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="OrderId_Txt"
-    private TextField OrderId_Txt; // Value injected by FXMLLoader
-
-    @FXML // fx:id="PricePerSeat_Txt"
-    private TextField PricePerSeat_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="PricePerSeatLabel"
+    private Label PricePerSeatLabel; // Value injected by FXMLLoader
 
     @FXML // fx:id="ReturnBackBtn"
     private Button ReturnBackBtn; // Value injected by FXMLLoader
 
-    @FXML // fx:id="SeatNumber_Txt"
-    private TextField SeatNumber_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="SeatNumberLabel"
+    private Label SeatNumberLabel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="TotalAmount_Txt"
-    private TextField TotalAmount_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="TotalAmountLabel"
+    private Label TotalAmountLabel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="TotalPrice_Txt"
-    private TextField TotalPrice_Txt; // Value injected by FXMLLoader
+    @FXML // fx:id="TotalPriceLabel"
+    private Label TotalPriceLabel; // Value injected by FXMLLoader
 
     @FXML // fx:id="cardNumber"
     private TextField cardNumber; // Value injected by FXMLLoader
@@ -64,7 +58,7 @@ public class Purchase {
     private TextField expiryDate; // Value injected by FXMLLoader
 
     @FXML
-    void ConfromPurchase(ActionEvent event) {
+    void ConfirmPurchase(ActionEvent event) {
 
     }
 
