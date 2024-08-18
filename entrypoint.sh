366a5db2377d69f2a1f363c0cd2866d67e778802
swalha1999 <mohamd514.m@gmail.com>
2024-08-18 21:45:26 +0300

    removed database from docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-# Start MySQL in the background
-mysqld --user=mysql --datadir=/var/lib/mysql &
-
-# Wait for MySQL to be fully up and running
-until mysqladmin ping -h 127.0.0.1 --silent; do
-  echo "Waiting for MySQL to start..."
-  sleep 10
-done
-
 # Start the Java application
 java -jar /app/application.jar
