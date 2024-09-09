c2712b95747c440344a12828217435673758f00e
abedassi2001 <Abedassi134@gmail.com>
2024-09-09 17:23:33 +0300

    support gui

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/SupportPage.java
@@ -0,0 +1,25 @@
+package il.cshaifasweng.OCSFMediatorExample.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class SupportPage {
+
+    @FXML
+    private TextArea issueDescription;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Label welcomeMessage;
+
+    @FXML
+    void handleSubmit(ActionEvent event) {
+
+    }
+
+}
