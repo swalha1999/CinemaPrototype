05f855a83ef9d7bebc68c3525a2abb857f91febd
swalha1999 <mohamd514.m@gmail.com>
2024-09-16 20:04:34 +0300

    added entity SupportTicket

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/dataTypes/SupportTicket.java
@@ -0,0 +1,102 @@
+package il.cshaifasweng.OCSFMediatorExample.entities.dataTypes;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "support_tickets")
+public class SupportTicket implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private String subject;
+    private String description;
+    private LocalDateTime createdDate;
+    private String status;
+
+    public SupportTicket() {
+        this.createdDate = LocalDateTime.now();
+        this.status = "Open"; // default status
+    }
+
+    public SupportTicket(String name, String email, String subject, String description) {
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+        this.description = description;
+        this.createdDate = LocalDateTime.now();
+        this.status = "Open";
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "SupportTicket{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", subject='" + subject + '\'' +
+                ", description='" + description + '\'' +
+                ", createdDate=" + createdDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
