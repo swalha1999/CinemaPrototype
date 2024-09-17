e55042a474d11e84440db77c1abbe3b11b03edf1
abedassi2001 <Abedassi134@gmail.com>
2024-09-17 23:51:17 +0300

    the combox in dashboard shows all the cinemas

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/DashBoard.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/DashBoard.java
@@ -1,5 +1,13 @@
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import il.cshaifasweng.OCSFMediatorExample.client.Client;
+import il.cshaifasweng.OCSFMediatorExample.client.data.CinemaView;
+import il.cshaifasweng.OCSFMediatorExample.client.data.SessionKeysStorage;
+import il.cshaifasweng.OCSFMediatorExample.client.events.GetAllCinemasEvent;
+import il.cshaifasweng.OCSFMediatorExample.entities.dataTypes.Cinema;
+import il.cshaifasweng.OCSFMediatorExample.entities.messages.Message;
+import il.cshaifasweng.OCSFMediatorExample.entities.messages.MessageType;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
@@ -8,8 +16,23 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DashBoard {
+
+    @FXML
+    public void initialize() throws IOException {
+        EventBus.getDefault().register(this);
+        System.out.println(locationComboi.getSelectionModel().getSelectedItem());
+        Message message = new Message(MessageType.GET_ALL_CINEMAS_REQUEST)
+                .setSessionKey(SessionKeysStorage.getInstance().getSessionKey());
+        Client.getClient().sendToServer(message);
+    }
 
     @FXML
     private PieChart ComplaintsTable;
@@ -27,16 +50,40 @@ public class DashBoard {
     private Button backBtn;
 
     @FXML
-    private ComboBox<?> locationComboi;
+    private ComboBox<CinemaView> locationComboi;
 
     @FXML
     void GoBack(ActionEvent event) {
-
+        // Logic to handle the "Back" button
     }
 
     @FXML
     void PickLocation(ActionEvent event) {
+        // Send a request to the server to get all cinema
 
     }
 
+    @Subscribe
+    public void onGetlocation(GetAllCinemasEvent event) {
+        // Extract the list of Cinema entities from the event
+        List<Cinema> cinemaList = event.getCinemas();
+
+        // Convert the list of Cinema to CinemaView
+        List<CinemaView> cinemaViewList = cinemaList.stream()
+                .map(CinemaView::new)  // Using the constructor that accepts Cinema
+                .collect(Collectors.toList());
+
+        // Add an "All Locations" option as a dummy CinemaView
+        CinemaView allLocations = new CinemaView(null, -1, "All Locations", "", "", "", "", "");
+        cinemaViewList.add(0, allLocations);  // Add "All Locations" at the top
+
+        // Set the list of CinemaView objects into the ComboBox
+        locationComboi.setItems(FXCollections.observableArrayList(cinemaViewList));
+
+        // Optionally, select the first item ("All Locations")
+        if (!cinemaViewList.isEmpty()) {
+            locationComboi.getSelectionModel().selectFirst();
+        }
+    }
 }
+
--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/data/CinemaView.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/data/CinemaView.java
@@ -13,20 +13,18 @@ public class CinemaView {
     private final SimpleStringProperty address;
     private final SimpleStringProperty phoneNumber;
     private final SimpleStringProperty email;
-    private  final SimpleStringProperty managerName;
+    private final SimpleStringProperty managerName;
 
-
-   public CinemaView(Cinema cinema) {
-       this.cinema = cinema;
-       id = new SimpleIntegerProperty(cinema.getId());
-       name = new SimpleStringProperty(cinema.getName());
-       city = new SimpleObjectProperty<>(cinema.getCity());
-       address = new SimpleStringProperty(cinema.getAddress());
-       phoneNumber = new SimpleStringProperty(cinema.getPhoneNumber());
-       email = new SimpleStringProperty(cinema.getEmail());
-       this.managerName = new SimpleStringProperty((cinema.getManager().getUsername()));
-   }
-
+    public CinemaView(Cinema cinema) {
+        this.cinema = cinema;
+        id = new SimpleIntegerProperty(cinema.getId());
+        name = new SimpleStringProperty(cinema.getName());
+        city = new SimpleObjectProperty<>(cinema.getCity());
+        address = new SimpleStringProperty(cinema.getAddress());
+        phoneNumber = new SimpleStringProperty(cinema.getPhoneNumber());
+        email = new SimpleStringProperty(cinema.getEmail());
+        managerName = new SimpleStringProperty(cinema.getManager().getUsername());
+    }
 
     // Parameterized Constructor
     public CinemaView(Cinema cinema, int id, String name, String city, String address, String phoneNumber, String email, String managerName) {
@@ -39,35 +37,41 @@ public class CinemaView {
         this.email = new SimpleStringProperty(email);
         this.managerName = new SimpleStringProperty(managerName);
     }
-    // Copy method
+
+    // Copy method to return a new instance
     public CinemaView copy(Cinema other) {
-        this.id.set(other.getId());
-        this.name.set(other.getName());
-        this.city.set(other.getCity());
-        this.address.set(other.getAddress());
-        this.phoneNumber.set(other.getPhoneNumber());
-        this.email.set(other.getEmail());
-        return this;
+        return new CinemaView(other);  // Create and return a new instance
     }
+
     // Getters
-   public SimpleIntegerProperty getId() {
+    public SimpleIntegerProperty getId() {
         return id;
-   }
-   public SimpleStringProperty getName() {
+    }
+
+    public SimpleStringProperty getName() {
         return name;
-   }
-   public SimpleObjectProperty<String> getCity() {
+    }
+
+    public SimpleObjectProperty<String> getCity() {
         return city;
-   }
-   public SimpleStringProperty getAddress() {
+    }
+
+    public SimpleStringProperty getAddress() {
         return address;
-   }
-   public SimpleStringProperty getPhoneNumber() {
+    }
+
+    public SimpleStringProperty getPhoneNumber() {
         return phoneNumber;
-   }
-   public SimpleStringProperty getEmail() {
+    }
+
+    public SimpleStringProperty getEmail() {
         return email;
-   }
+    }
+
+    public String getManagerName() {
+        return managerName.get();
+    }
+
     // Property methods for JavaFX bindings
     public SimpleIntegerProperty idProperty() {
         return id;
@@ -91,6 +95,10 @@ public class CinemaView {
 
     public SimpleStringProperty emailProperty() {
         return email;
+    }
+
+    public SimpleStringProperty managerNameProperty() {
+        return managerName;
     }
 
     // Setters with method chaining
@@ -124,19 +132,18 @@ public class CinemaView {
         return this;
     }
 
-    public String getManagerName() {
-        return managerName.get();
-    }
-    public SimpleStringProperty managerNameProperty() {
-       return managerName;
-    }
     public CinemaView setManagerName(String managerName) {
-       this.managerName.set(managerName);
-       return this;
+        this.managerName.set(managerName);
+        return this;
     }
 
     public Cinema getCinema() {
         return cinema;
     }
 
+    // Override toString for ComboBox display
+    @Override
+    public String toString() {
+        return name.get();  // This will display the cinema name in the ComboBox
+    }
 }
