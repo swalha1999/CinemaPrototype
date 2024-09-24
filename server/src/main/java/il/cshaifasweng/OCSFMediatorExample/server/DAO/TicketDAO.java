bf18c5d9c5c4d448284ac8c3a318ccc4c11e954e
swalha1999 <mohamd514.m@gmail.com>
2024-09-24 21:01:13 +0300

    added the logic of the return of the ticket

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/DAO/TicketDAO.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/DAO/TicketDAO.java
@@ -74,61 +74,79 @@ public class TicketDAO {
     }
 
     public Message removeTicket(Message request, LoggedInUser loggedInUser) {
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
+        // get the user from the request
+        // if the ticket has 3 or more hours before the screening, remove the ticket and add to the balance of the user full price
+        // if the ticket has less than 3 hours  and more than 1 hour before the screening, remove the ticket and add to the balance of the user half price
+        // else don't remove the ticket and return a message that the ticket cannot be removed
 
-            // Fetch the ticket to be deleted from the request
-            MovieTicket ticketToDelete = (MovieTicket) request.getDataObject();
+        Message response = new Message(MessageType.REMOVE_TICKET_RESPONSE);
 
-            // Retrieve the ticket from the database
-            MovieTicket ticket = session.get(MovieTicket.class, ticketToDelete.getId());
+        MovieTicket ticketToDelete = (MovieTicket) request.getDataObject();
+        MovieTicket ticket = session.get(MovieTicket.class, ticketToDelete.getId());
 
-            // Check if the ticket exists
-            if (ticket == null) {
-                if (transaction != null) transaction.rollback();
-                return new Message(MessageType.REMOVE_TICKET_RESPONSE)
-                        .setSuccess(false)
-                        .setMessage("Ticket not found");
-            }
-
-            // Retrieve the seat associated with this ticket
-            Seat seat = ticket.getSeat();
-
-            // Check if the seat exists and set its availability to true
-            if (seat != null) {
-                seat.setAvailable(true);
-                session.update(seat); // Update the seat's availability in the database
-            }
-
-            // Cancel the notification
-            removeNotification(ticket.getNotificationId());
-
-            // Delete the ticket
-            session.delete(ticket);
-
-            // Commit the transaction
-            transaction.commit();
-
-            // Fetch the updated list of tickets for the user
-            List<MovieTicket> updatedTickets = session.createQuery("from MovieTicket where user.id = :id", MovieTicket.class)
-                    .setParameter("id", loggedInUser.getUserId())
-                    .getResultList();
-
-            // Return a success message with the updated tickets
-            return new Message(MessageType.REMOVE_TICKET_RESPONSE)
-                    .setSuccess(true)
-                    .setMessage("Ticket removed successfully, seat is now available")
-                    .setDataObject(updatedTickets);
-
-        } catch (Exception e) {
-            // Rollback if there's any exception
-            if (transaction != null) transaction.rollback();
-            e.printStackTrace(); // Log the exception for debugging
-            return new Message(MessageType.REMOVE_TICKET_RESPONSE)
+        if (ticket == null) {
+            return response
                     .setSuccess(false)
-                    .setMessage("An error occurred while removing the ticket");
+                    .setMessage("Ticket not found");
         }
+
+        if (ticket.getIsUsed()) {
+            return response
+                    .setSuccess(false)
+                    .setMessage("Ticket already used");
+        }
+
+        if (ticket.getRefunded()) {
+            return response
+                    .setSuccess(false)
+                    .setMessage("Ticket already refunded");
+        }
+
+        if (ticket.getBundleTicket()) {
+            return response
+                    .setSuccess(false)
+                    .setMessage("Ticket is part of a bundle cannot be refunded");
+        }
+
+        // at this point we know that the ticket can be refunded
+        // we free the seat to be available again
+        Seat seat = ticket.getSeat();
+        seat.setAvailable(true);
+        removeNotification(ticket.getNotificationId());
+
+        User user = ticket.getUser();
+        if (user == null) {
+            return response
+                    .setSuccess(false)
+                    .setMessage("User not found");
+        }
+
+        LocalDateTime screeningTime = ticket.getScreening().getStartingAt();
+        LocalDateTime currentTime = LocalDateTime.now();
+        long hoursUntilScreening = java.time.Duration.between(currentTime, screeningTime).toHours();
+
+        if (hoursUntilScreening >= 3) {
+            // Full refund
+            user.setBalance(user.getBalance() + ticket.getScreening().getPrice());
+        } else if (hoursUntilScreening >= 1) {
+            // Half refund
+            user.setBalance(user.getBalance() + ticket.getScreening().getPrice() / 2);
+        } else {
+            return response
+                    .setSuccess(false)
+                    .setMessage("Ticket cannot be refunded less than 1 hour before the screening");
+        }
+
+        session.beginTransaction();
+        session.delete(ticket);
+        session.update(user);
+        session.update(seat);
+        session.getTransaction().commit();
+
+        return response
+                .setSuccess(true)
+                .setMessage("Ticket removed successfully");
+
     }
 
     public Message purchaseTickets(Message request, LoggedInUser loggedInUser) {
