807ccd2c019093072dbab051672fddcf644348cd
swalha1999 <mohamd514.m@gmail.com>
2024-09-26 00:27:06 +0300

    added the auto id

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/dataTypes/PriceChangeRequest.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/dataTypes/PriceChangeRequest.java
@@ -10,7 +10,7 @@ public class PriceChangeRequest implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "screening_id")
