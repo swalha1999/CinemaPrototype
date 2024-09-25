7435adfbd7cda9d413a6cd814c7a3a1f6c2e58dc
Mahmoud <mabade@campus.haifa.ac.il>
2024-09-25 19:44:25 +0300

    fix a edit screening and edit price bug

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/EditPrice.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/EditPrice.java
@@ -1,11 +1,19 @@
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import il.cshaifasweng.OCSFMediatorExample.client.events.ShowSideUIEvent;
+import il.cshaifasweng.OCSFMediatorExample.entities.dataTypes.Movie;
+import il.cshaifasweng.OCSFMediatorExample.entities.dataTypes.Screening;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.Objects;
 
 public class EditPrice {
+
 
     @FXML
     private Button ConfirmButton;
@@ -15,6 +23,20 @@ public class EditPrice {
 
     @FXML
     void EditPrice(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void initialize() {
+        EventBus.getDefault().register(this); //TODO: add this to all controllers - please :)
+    }
+
+    @Subscribe
+    public void onSideUiChange (ShowSideUIEvent event){
+        if(!Objects.equals(event.getUIName(), "EditPrice")){
+            return;
+        }
+
 
     }
 
